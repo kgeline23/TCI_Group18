03fa4751f1f58f520c2cfb316aace652f5a9207e
Kruit,Eveline E <e.kruit@student.fontys.nl>
2021-11-01 03:54:43 +0100

    implement simpleteacherservice

--- tci-groupassignment-efit/src/main/java/services/exposed/teacher/SimpleTeacherService.java
+++ tci-groupassignment-efit/src/main/java/services/exposed/teacher/SimpleTeacherService.java
@@ -1,5 +1,63 @@
 package services.exposed.teacher;
 
+import model.ExamID;
+import model.StudentExam;
+import services.exposed.ExamNotFoundException;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
 public class SimpleTeacherService implements TeacherInterface {
     // TODO
+
+    /**
+     *this method creates an examsetup
+     * @param examname
+     * @param begindate
+     * @return
+     * @throws DuplicateExamException
+     * @throws IllegalArgumentException
+     * @should throw IllegalArgumentException when examname is null
+     */
+    @Override
+    public ExamID createExamSetup(String examname, long begindate) throws DuplicateExamException,IllegalArgumentException {
+        ExamID examID = new ExamID(examname,begindate);
+        return examID;
+    }
+
+    @Override
+    public Set<ExamID> getOpenExams() {
+        return null;
+    }
+
+    @Override
+    public Set<ExamID> getOpenExams(Date dateOnOrAfter, Date dateOnOrBefore) {
+        return null;
+    }
+
+    @Override
+    public void addExamMaterial(ExamID exam, Object examMaterial) throws ExamNotFoundException, ExamStartedException {
+
+    }
+
+    @Override
+    public List<Object> getExamMaterials(ExamID exam) {
+        return null;
+    }
+
+    @Override
+    public boolean removeExamMaterial(ExamID exam, Object examMaterial) throws ExamNotFoundException, ExamStartedException {
+        return false;
+    }
+
+    @Override
+    public Set<StudentExam> getExamResults(ExamID exam) throws ExamNotFoundException, ExamNotEndedException {
+        return null;
+    }
+
+    @Override
+    public boolean finalizeExam(ExamID examID) throws ExamNotFoundException {
+        return false;
+    }
 }
